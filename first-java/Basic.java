025a152bce4f63d978ae3c458159457d9f4b1125
Jay Purohit <180675177+Jay-code-09@users.noreply.github.com>
2026-01-02 19:49:28 +0530

    Basics of java
    Added a main method with a while loop that prints numbers from 1 to 4.

---
+++ first-java/Basic.java
@@ -0,0 +1,18 @@
+void main() {
+//    int a = 20;
+//    if(a==10){
+//        System.out.println(a);
+//    }else{
+//        System.out.println("Not int");
+//    }
+    int c = 1;
+    while(c != 5){
+        System.out.println(c);
+        c++;
+    }
+
+    // for loop
+//    for(c=1; c!=5 ; c++){
+//        System.out.println(c);
+//    }
+}
